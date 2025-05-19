4f959a8a8fdfcaa93981f71f290fb49e569aeb31
solgiuria <mariasolgiuria@gmail.com>
2025-05-19 14:48:03 -0300

    Primer commit: proyecto agregado a github

---
+++ tpFinal/src/main/java/com/reportalo/tpFinal/TpFinalApplication.java
@@ -0,0 +1,13 @@
+package com.reportalo.tpFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}

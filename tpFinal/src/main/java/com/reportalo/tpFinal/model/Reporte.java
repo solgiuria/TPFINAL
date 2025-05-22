06058f344b9b16416544b5c4e289fae32adcc3e6
solgiuria <mariasolgiuria@gmail.com>
2025-05-22 18:20:26 -0300

    relacio hecha entre usuario y reporte

--- tpFinal/src/main/java/com/reportalo/tpFinal/model/Reporte.java
+++ tpFinal/src/main/java/com/reportalo/tpFinal/model/Reporte.java
@@ -20,6 +20,10 @@ public class Reporte {
     @JoinColumn(name = "id_subTipoReporte") //el nombre de la columna en mysql
     private Subtipo_Reporte subtipo; //el nombre del atributo en java al cual se asocia la fk
 
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
 
     private String descripcion;
     private LocalDateTime fecha_hora;

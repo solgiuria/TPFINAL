828d96bca019aac3d5f45721589997fb15a6091f
solgiuria <mariasolgiuria@gmail.com>
2025-05-22 14:45:00 -0300

    se agrego entidad logginRequest con su controller y servicio

--- tpFinal/src/main/java/com/reportalo/tpFinal/model/Reporte.java
+++ tpFinal/src/main/java/com/reportalo/tpFinal/model/Reporte.java
@@ -1,6 +1,28 @@
 package com.reportalo.tpFinal.model;
+import com.reportalo.tpFinal.enums.EstadoReporte;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Data //incluye @getter @setter @toString @HashCode @equals
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Reportes")
 public class Reporte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id ;
-    private int id_Tipo_Reporte;
+
+    @ManyToOne
+    @JoinColumn(name = "id_subTipoReporte") //el nombre de la columna en mysql
+    private Subtipo_Reporte subtipo; //el nombre del atributo en java
+
+
+    private String descripcion;
+    private LocalDateTime fecha_hora;
+    private String ubicacion;
+    private EstadoReporte estado;
 }

6a657da5969fa1b6d7171e3b1c2b0ef6d55ebbe8
solgiuria <mariasolgiuria@gmail.com>
2025-05-22 14:45:00 -0300

    se agrego entidad logginRequest con su controller y servicio

--- tpFinal/src/main/java/com/reportalo/tpFinal/model/Subtipo_Reporte.java
+++ tpFinal/src/main/java/com/reportalo/tpFinal/model/Subtipo_Reporte.java
@@ -1,4 +1,26 @@
 package com.reportalo.tpFinal.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data //incluye @getter @setter @toString @HashCode @equals
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Subtipo_Reporte")
+
 public class Subtipo_Reporte {
+
+    @OneToMany(mappedBy = "subtipo", cascade = CascadeType.ALL) //cualq modif a esta tabla afecta a reporte
+    private Long id;
+
+    @ManyToOne //subtipo puede tener asociado solo un tipo
+    @JoinColumn(name = "id_tipoReporte")
+    private Tipo_Reporte tipo_reporte;
+
+
 }

39d7d90d377513e19223be8e7ca34745c3280863
TomassAlberti <alberti3113@gmail.com>
2025-05-22 09:06:02 -0300

    Se conecto la clase Tipo_reporte con la clase reporte, ademas se agregaron los atributos de la misma y de la clase Usuario

--- tpFinal/src/main/java/com/reportalo/tpFinal/model/Tipo_Reporte.java
+++ tpFinal/src/main/java/com/reportalo/tpFinal/model/Tipo_Reporte.java
@@ -1,4 +1,30 @@
 package com.reportalo.tpFinal.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.List;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Tipos_Reportes")
 public class Tipo_Reporte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Size(min = 2, max = 20, message = "El nombre debe tener entre 3 y 20 caracteres")
+    private String nombre;
+
+
+    @OneToMany(mappedBy = "tipo_reporte", cascade = CascadeType.ALL) //un tipo puede estar asociado a muchos subtipos
+    private Long id;
+
 }

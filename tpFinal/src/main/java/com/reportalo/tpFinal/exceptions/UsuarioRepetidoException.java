670079aef93ecc9a1fd89867ba75df1531a4204f
solgiuria <mariasolgiuria@gmail.com>
2025-05-27 23:53:51 -0300

    se hizo metodo de registro servicio para usuario y user de spring security, + excep personalizada para usuario repetido

---
+++ tpFinal/src/main/java/com/reportalo/tpFinal/exceptions/UsuarioRepetidoException.java
@@ -0,0 +1,7 @@
+package com.reportalo.tpFinal.exceptions;
+
+public class UsuarioRepetidoException extends RuntimeException {
+    public UsuarioRepetidoException(String message) {
+        super(message);
+    }
+}
